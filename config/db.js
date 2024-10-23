aaa21420ac8a80544ec0e2c30e616fb609231813
IvanOrjuela13 <167380738+IvanOrjuela13@users.noreply.github.com>
2024-10-23 04:05:03 -0500

    Update db.js

--- config/db.js
+++ config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        await mongoose.connect('mongodb+srv://Ivan:1997@cluster0.8vwpk.mongodb.net/yourdbname?retryWrites=true&w=majority');
+        await mongoose.connect('mongodb+srv://Ivan:1997@cluster0.8vwpk.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0');
         console.log('MongoDB connected');
     } catch (error) {
         console.error('Error connecting to MongoDB:', error);
