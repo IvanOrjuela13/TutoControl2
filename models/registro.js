ce91a1492fb967b8dbad6c2e53480737d83a9600
IvanOrjuela13 <167380738+IvanOrjuela13@users.noreply.github.com>
2024-12-23 14:14:40 -0500

    Update registro.js

--- models/registro.js
+++ models/registro.js
@@ -8,7 +8,8 @@ const registroSchema = new mongoose.Schema({
         lng: { type: Number, required: true }
     },
     fecha: { type: Date, default: Date.now }, // Se sobrescribe con obtenerFechaLocal()
-    tipo: { type: String, required: true } // 'entrada' o 'salida'
+    tipo: { type: String, required: true }, // 'entrada' o 'salida'
+    image: { type: String, required: true } // Almacena la imagen en formato Base64
 });
 
 module.exports = mongoose.model('Registro', registroSchema);
