dd924186373cfd94efb4b4dd8090fdc2773bce76
IvanOrjuela13 <167380738+IvanOrjuela13@users.noreply.github.com>
2024-12-23 15:09:53 -0500

    Update registro.js

--- routes/registro.js
+++ routes/registro.js
@@ -5,26 +5,19 @@ const router = express.Router();
 
 // Ruta para registrar entrada
 router.post('/entrada', async (req, res) => {
-    const { userId, deviceID, ubicacion, image } = req.body;
+    const { userId, deviceID, ubicacion } = req.body;
 
     try {
-        // Verificar si el campo `image` está presente
-        if (!image) {
-            return res.status(400).json({ msg: 'La imagen es obligatoria' });
-        }
-
-        // Establece la fecha y hora actual en la zona horaria de Bogotá
+        // Establece la fecha y hora actual en la zona horaria de Bogotá y ajusta la conversión a UTC
         let fechaLocal = moment.tz("America/Bogota").subtract(5, 'hours');
 
         const nuevoRegistro = new Registro({
             userId,
             deviceID,
             ubicacion,
-            fecha: fechaLocal.toDate(),
-            tipo: 'entrada',
-            image // Guardar la imagen en Base64
+            fecha: fechaLocal.toDate(), // Usa la fecha ajustada en Bogotá
+            tipo: 'entrada'
         });
-
         await nuevoRegistro.save();
         res.status(201).json({ msg: 'Entrada registrada exitosamente' });
     } catch (error) {
@@ -35,26 +28,19 @@ router.post('/entrada', async (req, res) => {
 
 // Ruta para registrar salida
 router.post('/salida', async (req, res) => {
-    const { userId, deviceID, ubicacion, image } = req.body;
+    const { userId, deviceID, ubicacion } = req.body;
 
     try {
-        // Verificar si el campo `image` está presente
-        if (!image) {
-            return res.status(400).json({ msg: 'La imagen es obligatoria' });
-        }
-
-        // Establece la fecha y hora actual en la zona horaria de Bogotá
+        // Establece la fecha y hora actual en la zona horaria de Bogotá y ajusta la conversión a UTC
         let fechaLocal = moment.tz("America/Bogota").subtract(5, 'hours');
 
         const nuevoRegistro = new Registro({
             userId,
             deviceID,
             ubicacion,
-            fecha: fechaLocal.toDate(),
-            tipo: 'salida',
-            image // Guardar la imagen en Base64
+            fecha: fechaLocal.toDate(), // Usa la fecha ajustada en Bogotá
+            tipo: 'salida'
         });
-
         await nuevoRegistro.save();
         res.status(201).json({ msg: 'Salida registrada exitosamente' });
     } catch (error) {
