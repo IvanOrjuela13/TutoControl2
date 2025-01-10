15d0426ab3aad995b74fcf2a8e04ce9c04132b3c
IvanOrjuela13 <167380738+IvanOrjuela13@users.noreply.github.com>
2025-01-10 18:49:02 -0500

    Update registro.js

--- routes/registro.js
+++ routes/registro.js
@@ -1,10 +1,32 @@
 const express = require('express');
+const multer = require('multer');
 const Registro = require('../models/registro');
 const moment = require('moment-timezone');
 const router = express.Router();
 
+// Configuración de Multer para almacenar los archivos
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/'); // Carpeta donde se guardarán los archivos
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + file.originalname); // Nombre del archivo con su extensión
+    }
+});
+
+// Filtrar los tipos de archivos permitidos (por ejemplo, imágenes)
+const fileFilter = (req, file, cb) => {
+    if (file.mimetype.startsWith('image/')) {
+        cb(null, true); // Aceptar el archivo si es una imagen
+    } else {
+        cb(new Error('Solo se permiten imágenes'), false); // Rechazar el archivo si no es una imagen
+    }
+};
+
+const upload = multer({ storage: storage, fileFilter: fileFilter });
+
 // Ruta para registrar entrada
-router.post('/entrada', async (req, res) => {
+router.post('/entrada', upload.single('archivo'), async (req, res) => {
     const { userId, deviceID, ubicacion } = req.body;
 
     try {
@@ -15,9 +37,11 @@ router.post('/entrada', async (req, res) => {
             userId,
             deviceID,
             ubicacion,
+            archivo: req.file ? req.file.path : null, // Guarda el path del archivo si existe
             fecha: fechaLocal.toDate(), // Usa la fecha ajustada en Bogotá
             tipo: 'entrada'
         });
+
         await nuevoRegistro.save();
         res.status(201).json({ msg: 'Entrada registrada exitosamente' });
     } catch (error) {
@@ -27,7 +51,7 @@ router.post('/entrada', async (req, res) => {
 });
 
 // Ruta para registrar salida
-router.post('/salida', async (req, res) => {
+router.post('/salida', upload.single('archivo'), async (req, res) => {
     const { userId, deviceID, ubicacion } = req.body;
 
     try {
@@ -38,9 +62,11 @@ router.post('/salida', async (req, res) => {
             userId,
             deviceID,
             ubicacion,
+            archivo: req.file ? req.file.path : null, // Guarda el path del archivo si existe
             fecha: fechaLocal.toDate(), // Usa la fecha ajustada en Bogotá
             tipo: 'salida'
         });
+
         await nuevoRegistro.save();
         res.status(201).json({ msg: 'Salida registrada exitosamente' });
     } catch (error) {
