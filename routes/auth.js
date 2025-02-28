a4e0535283e34c57d74555c6dce1535fe99e5a6b
IvanOrjuela13 <167380738+IvanOrjuela13@users.noreply.github.com>
2025-02-28 01:41:28 -0500

    Update auth.js

--- routes/auth.js
+++ routes/auth.js
@@ -1,7 +1,6 @@
 const express = require('express');
 const User = require('../models/User');
 const jwt = require('jsonwebtoken');
-const bcrypt = require('bcryptjs');
 const router = express.Router();
 
 // Ruta para restablecer la contraseña
@@ -9,26 +8,16 @@ router.post('/reset-password', async (req, res) => {
     const { cedula, newPassword } = req.body;
 
     try {
-        // Buscar al usuario por cédula
         const user = await User.findOne({ cedula });
 
         if (!user) {
             return res.status(404).json({ msg: 'Usuario no encontrado' });
         }
 
-        // Validar que la nueva contraseña sea válida
-        if (!newPassword || newPassword.length < 6) {
-            return res.status(400).json({ msg: 'La contraseña debe tener al menos 6 caracteres' });
-        }
-
-        // Encriptar la nueva contraseña
-        const salt = await bcrypt.genSalt(10);
-        user.password = await bcrypt.hash(newPassword, salt);
-
-        // Guardar el usuario con la nueva contraseña encriptada
+        // Encriptar la nueva contraseña antes de guardarla
+        user.password = newPassword;
         await user.save();
 
-        // Enviar respuesta de éxito
         res.json({ msg: 'Contraseña actualizada exitosamente' });
 
     } catch (error) {
@@ -56,15 +45,11 @@ router.post('/register', async (req, res) => {
             return res.status(400).json({ msg: 'Ya hay un usuario registrado en este dispositivo' });
         }
 
-        // Encriptar la contraseña
-        const salt = await bcrypt.genSalt(10);
-        const hashedPassword = await bcrypt.hash(password, salt);
-
         const newUser = new User({
             fullName,
             cedula,
             area,
-            password: hashedPassword,
+            password,
             deviceID
         });
 
@@ -88,7 +73,8 @@ router.post('/login', async (req, res) => {
             return res.status(400).json({ msg: 'Usuario no encontrado' });
         }
 
-        const isMatch = await bcrypt.compare(password, user.password);
+        // Verificar la contraseña usando el método matchPassword
+        const isMatch = await user.matchPassword(password);
         if (!isMatch) {
             return res.status(400).json({ msg: 'Contraseña incorrecta' });
         }
