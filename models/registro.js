d0bfe24aae5b7c5da866fc7f133b1c588a952daa
IvanOrjuela13 <167380738+IvanOrjuela13@users.noreply.github.com>
2025-03-02 20:47:25 -0500

    Update registro.js

--- models/registro.js
+++ models/registro.js
@@ -2,6 +2,7 @@ const mongoose = require('mongoose');
 
 const registroSchema = new mongoose.Schema({
     userId: { type: String, required: true },
+    deviceID: { type: String, required: true },
     ubicacion: {
         lat: { type: Number, required: true },
         lng: { type: Number, required: true }
