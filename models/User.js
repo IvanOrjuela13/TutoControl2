fcf0ae9744101146ea404d538cdee4cca5a47b7f
IvanOrjuela13 <167380738+IvanOrjuela13@users.noreply.github.com>
2025-03-02 21:36:49 -0500

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,20 +1,24 @@
-const mongoose = require('mongoose');
+models/User.js  const mongoose = require('mongoose');
 const bcrypt = require('bcryptjs');
 
 const UserSchema = new mongoose.Schema({
     fullName: { type: String, required: true },
-    cedula: { type: String, required: true, unique: true },
+    cedula: { type: String, required: true, unique: true },  // La cédula sigue siendo única
     area: { type: String, required: true },
     password: { type: String, required: true },
-    deviceID: { type: String }
+    deviceID: { type: String, required: true }  // Se eliminó `unique: true`
 });
 
+// Método para comparar contraseñas
 UserSchema.methods.matchPassword = async function (password) {
     return await bcrypt.compare(password, this.password);
 };
 
+// Middleware para encriptar la contraseña antes de guardar
 UserSchema.pre('save', async function (next) {
-    if (!this.isModified('password')) return next();
+    if (!this.isModified('password')) {
+        return next();
+    }
     const salt = await bcrypt.genSalt(10);
     this.password = await bcrypt.hash(this.password, salt);
     next();
